003b510dc2d30fff335cc3894aa6c7b1ae72750e
Trí Nguyễn Minh <nmtri17@apcs.vn>
2019-05-28 13:10:41 +0700

    first commit

---
+++ Assets/Scripts/BackgroundCreate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCreate : MonoBehaviour
+{
+    public Vector2 startPoint;
+    public Vector2 offSet;
+    public Vector2 size;
+    public GameObject candy;
+    void Start() {
+
+        for (int i = 0; i < size.y; i++)
+        {
+            for (int j = 0; j < size.x; j++) {
+                Instantiate(candy,
+                    new Vector2(startPoint.x + offSet.x*j,startPoint.y - offSet.y*i),
+                    Quaternion.identity
+                );             
+                Debug.Log(startPoint.x + offSet.x*j);
+                Debug.Log(startPoint.y - offSet.y*i);
+            }
+            
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Candy : MonoBehaviour
+{
+    public Sprite[] SpriteList;
+    public Sprite[] ConSpriteList;
+    // Start is called before the first frame update
+    public float maxChangeTime = 2f;
+    float time = 2;
+    int i;
+    float changeTime;
+    void Awake() {
+        i = Random.Range(0,SpriteList.Length);
+        changeTime = Random.RandomRange(0f,maxChangeTime);
+        GetComponent<SpriteRenderer>().sprite = SpriteList[i];
+    }
+
+
+
+    void Start()
+    {
+        time = changeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time > 0) {
+            GetComponent<SpriteRenderer>().sprite = SpriteList[i];
+        }
+        else {
+            GetComponent<SpriteRenderer>().sprite = ConSpriteList[i];
+        }
+
+        if (time < -2* changeTime) time = changeTime;
+
+
+        
+    }
+}
